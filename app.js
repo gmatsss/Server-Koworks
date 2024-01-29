c689433a44ccf5633b31644aee1b32ba15c2a3a5
Your Name~ <gmaturan60@gmail.com>
2024-01-29 14:43:19 +0800

    update

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-      // sameSite: "strict", // Allow cookies on cross-origin requests
-      // secure: false, // Normally requires HTTPS, but set to false for HTTP in development
+      sameSite: "none", // Allow cookies on cross-origin requests
+      secure: false, // Normally requires HTTPS, but set to false for HTTP in development
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
   })
